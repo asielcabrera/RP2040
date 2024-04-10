e1e811916080dee43045f94228bc8de23fd4e6ae
Asiel Cabrera <cabrerasiel@gmail.com>
2024-04-10 19:16:24 -0400

    change some stuff

--- Package.swift
+++ Package.swift
@@ -4,24 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "RP2024",
+    name: "RP2040",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RP2024",
-            type: .static,
-            targets: ["RP2024"]),
+            targets: ["RP2040"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "RP2024",
-            dependencies: ["Support"]
-        ),
         .target(name: "Support"),
+        .target(name: "RP2040", dependencies: ["Support"]),
         .testTarget(
-            name: "RP2024Tests",
-            dependencies: ["RP2024"]),
+            name: "RP2040Tests",
+            dependencies: ["RP2040"]),
     ]
 )
